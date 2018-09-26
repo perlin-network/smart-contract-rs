5ab48941fe6cc3043c857b13c11678c37c3f9544
losfair <zhy20000919@hotmail.com>
2018-09-26 23:18:21 +0800

    sys: API bindings

---
+++ src/sys.rs
@@ -0,0 +1,8 @@
+extern "C" {
+    pub fn _reason_len() -> usize;
+    pub fn _reason(out: *mut u8);
+    pub fn _get_len(key: *const u8, key_len: usize) -> usize;
+    pub fn _get(key: *const u8, key_len: usize, value_out: *mut u8);
+    pub fn _set(key: *const u8, key_len: usize, value: *const u8, value_len: usize);
+    pub fn _send_transaction(tag: *const u8, tag_len: usize, payload: *const u8, payload_len: usize);
+}
