73fa65928a412feb8f6c9de4a5c898829571ad6e
losfair <zhy20000919@hotmail.com>
2018-09-26 23:18:32 +0800

    reason: Activation reason loading

---
+++ src/reason.rs
@@ -0,0 +1,18 @@
+#[derive(Deserialize)]
+pub struct Reason<T> {
+    pub kind: String,
+    pub details: T,
+}
+
+impl<T: for<'a> ::serde::Deserialize<'a>> Reason<T> {
+    pub fn load() -> Option<Reason<T>> {
+        let raw_len = unsafe { ::sys::_reason_len() };
+        let mut raw = Vec::with_capacity(raw_len);
+        unsafe { raw.set_len(raw_len) };
+        unsafe { ::sys::_reason(raw.as_mut_ptr()) };
+        match ::serde_json::from_slice(&raw) {
+            Ok(v) => Some(v),
+            Err(_) => None,
+        }
+    }
+}
