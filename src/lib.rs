25e6540c0bf589b4ef8d20411a9da155bd6a2d6b
losfair <zhy20000919@hotmail.com>
2018-11-08 15:55:18 +0800

    lib: Add contract_entry macro

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,23 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
-pub mod sys;
-pub mod reason;
 pub mod persistent;
+pub mod reason;
+pub mod sys;
 pub mod transaction;
 
 pub use reason::Reason;
 pub use transaction::Transaction;
+
+#[macro_export]
+macro_rules! contract_entry {
+    ($entry:ident) => {
+        #[no_mangle]
+        pub extern "C" fn contract_main() {
+            fn assert_entry_type<F: Fn()>(_: F) {}
+            assert_entry_type($entry);
+
+            $entry();
+        }
+    };
+}
