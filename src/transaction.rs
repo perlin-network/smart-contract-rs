bb7b82a3e2a7ec6a987c0623b3ce2fb79598032a
losfair <zhy20000919@hotmail.com>
2018-11-13 20:24:44 +0800

    transaction: Add builtin constructs for transfer.

--- src/transaction.rs
+++ src/transaction.rs
@@ -4,6 +4,12 @@ pub struct Transaction {
     pub payload: Vec<u8>,
 }
 
+#[derive(Serialize)]
+pub struct TransferTx<'a> {
+    pub amount: u64,
+    pub recipient: &'a str,
+}
+
 impl Transaction {
     pub fn new<K: Into<String>>(tag: K, payload: Vec<u8>) -> Transaction {
         Transaction {
@@ -23,6 +29,19 @@ impl Transaction {
         let tag = self.tag.as_bytes();
         let payload = self.payload.as_slice();
 
-        unsafe { ::sys::_send_transaction(tag.as_ptr(), tag.len(), payload.as_ptr(), payload.len()) };
+        unsafe {
+            ::sys::_send_transaction(tag.as_ptr(), tag.len(), payload.as_ptr(), payload.len())
+        };
     }
+}
+
+pub fn transfer(recipient: &str, amount: u64) {
+    Transaction::new_json(
+        "transfer",
+        TransferTx {
+            amount: amount,
+            recipient: recipient,
+        },
+    )
+    .send();
 }
