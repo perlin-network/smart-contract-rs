16636d2746ad80c98ae32b846d2dc7d2b3f53938
Ahmad Muzakkir <muzakkir@terato.com>
2019-03-27 20:46:14 +0800

    crypto: function _sign now returns a value, 0 failed, 1 success

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,5 +1,5 @@
 extern "C" {
-    pub fn _sign(key: *const u8, key_len: usize, msg: *const u8, msg_len: usize, sign_out: *mut u8);
+    pub fn _sign(key: *const u8, key_len: usize, msg: *const u8, msg_len: usize, sign_out: *mut u8) -> usize;
     pub fn _verify(pubkey: *const u8, pubkey_len: usize, msg: *const u8, msg_len: usize, sign: *const u8, sign_len: usize) -> usize;
     pub fn _hash(data: *const u8, data_len: usize, hash_out: *mut u8);
 }
