f6d03b4cc3d7770e579bab666d2e2bdf4d8af5d0
Kenta Iwasaki <kenta@perlin.net>
2019-06-24 16:27:48 +0900

    tx: update transfer and contract payload format

--- smart-contract/src/transaction.rs
+++ smart-contract/src/transaction.rs
@@ -21,6 +21,7 @@ pub trait Transaction: Writeable + Readable {
     fn tag(&self) -> TransactionTag;
 }
 
+#[derive(Default)]
 pub struct Transfer {
     pub destination: [u8; 32],
     pub amount: u64,
@@ -34,20 +35,35 @@ impl Writeable for Transfer {
         self.destination.write_to(buffer);
         self.amount.write_to(buffer);
 
-        self.func_name.write_to(buffer);
-        self.func_params.write_to(buffer);
+        if self.func_name.len() > 0 && self.func_params.len() > 0 {
+            0u64.write_to(buffer); // Specify an empty gas limit.
+
+            self.func_name.write_to(buffer);
+            self.func_params.write_to(buffer);
+        }
     }
 }
 
 impl Readable for Transfer {
     fn read_from(buffer: &[u8], pos: &mut u64) -> Transfer {
-        Transfer {
-            destination: <[u8; 32]>::read_from(buffer, pos),
-            amount: u64::read_from(buffer, pos),
+        let mut params = Transfer::default();
 
-            func_name: Vec::<u8>::read_from(buffer, pos),
-            func_params: Vec::<u8>::read_from(buffer, pos),
+        params.destination = <[u8; 32]>::read_from(buffer, pos);
+        params.amount = u64::read_from(buffer, pos);
+
+        if *pos < buffer.len() as u64 {
+            u64::read_from(buffer, pos); // Read gas limit.
         }
+
+        if *pos < buffer.len() as u64 {
+            params.func_name = Vec::<u8>::read_from(buffer, pos); // Read function name.
+        }
+
+        if *pos < buffer.len() as u64 {
+            params.func_params = Vec::<u8>::read_from(buffer, pos) // Read function params.
+        }
+
+        params
     }
 }
 
@@ -57,22 +73,28 @@ impl Transaction for Transfer {
     }
 }
 
+#[derive(Default)]
 pub struct Contract {
+    pub payload: Vec<u8>,
     pub code: Vec<u8>,
 }
 
 impl Writeable for Contract {
     fn write_to(&self, buffer: &mut Vec<u8>) {
+        self.payload.write_to(buffer);
         buffer.append(&mut self.code.clone());
     }
 }
 
 impl Readable for Contract {
     fn read_from(buffer: &[u8], pos: &mut u64) -> Contract {
+        let mut params = Contract::default();
+
+        params.payload = Vec::<u8>::read_from(buffer, pos);
+        params.code = buffer[*pos as usize..].to_vec();
+
         *pos = buffer.len() as u64;
 
-        Contract {
-            code: buffer.to_vec(),
-        }
+        params
     }
 }
